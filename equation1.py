5707ac261fc7dd7c5e3c980be98957bf298174a2
Sergey Garanin <wwwser11@yandex.ru>
2020-11-19 20:50:00 +0300

    Добавил возможность ввода чисел пользователю

--- equation1.py
+++ equation1.py
@@ -1,10 +1,10 @@
 # уравнение прямой вида y = kx + b задано в виде строки.
 # Определить координату y точки с заданной координатой x.
-equation = 'y = 10x + 105.5'
-x = 1
-
+user_numbers = input('Сейчас мы решим уравнение y = kx + b. \nВедите 2 числа(k, b) через запятую: ' )
+user_numbers1 = user_numbers.split(',')
+equation = f'y = {user_numbers1[0]}x + {user_numbers1[1]}'
+x = float(input('Введите х: ' ))
 list_equation = equation.split(' ')
-print(list_equation)
 b = float(list_equation[-1])
 for element in list_equation:
     if "x" in element:
